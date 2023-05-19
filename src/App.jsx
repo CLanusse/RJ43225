8ca4ff4d3403328bc5fc3ddb848d57b1dfa5e608
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-05-18 21:12:31 -0300

    jsx y bundler

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Tutores } from './Tutores'
+// import { Profe } from './Profe'
+// import { despedirse, saludar } from './Utilidades.js'
+// import abc from './Utilidades.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // saludar()
+  // despedirse()
+  
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hola Mundo!</h1>
-      <ul>
-        <li>Carlos</li>
-        <li>Juan</li>
-        <li>Lucrecia</li>
-      </ul>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Bienvenidos a ReactJS
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {/* <Profe /> */}
+      <Tutores />
+    </div>
+    
   )
 }
 
