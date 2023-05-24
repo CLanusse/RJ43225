60b8836b3d18130e7d18257bae3b69de92bdfaf9
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-05-24 20:06:52 -0300

    after 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { Usuario } from "./components/Usuario"
-import { Clicker } from "./ejemplos/Clicker"
+import Header from "./components/Header/Header"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from "react-bootstrap"
 
 function App() {
 
+  const clickear = () => console.log("Hola mundo!")
   
   return (
     <div>
-      <h2>React JS</h2>
-      <hr/>
+      <Header />
+      <ItemListContainer mensaje="Hola mundo!"/>
 
-      <Clicker init={15} step={5}/>
-      {/* <Clicker init={50} step={2}/>
-      <Clicker /> */}
-
-      {/* <Usuario nombre="Lucia Dias" curso="Javascript" edad={21} estado="Activo" rol="Tutor"/>
-      <Usuario nombre="Lautaro Zalazar" curso="React" edad={24} estado="Inactivo" rol="Estudiante"/>
-      <Usuario nombre={profe.nombre} curso={profe.curso} edad={profe.edad} estado="Activo" rol={profe.rol}/> */}
+      <div className="container">
+        <Button onClick={clickear} size="lg" variant="warning">Click me</Button>
+        <Button variant="success">Click me</Button>
+        <Button size="sm" variant="danger">Click me</Button>
+      </div>
     </div>
-    
+   
   )
 }
 
