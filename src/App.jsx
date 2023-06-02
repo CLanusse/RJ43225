c61c6e914f6273dfb3113fdf454726ba61414273
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-06-01 21:04:07 -0300

    Clase 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import Header from "./components/Header/Header"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button } from "react-bootstrap"
-import MiContenedor from "./ejemplos/MiContenedor"
-import MiBoton from "./ejemplos/MiBoton/MiBoton"
-import { Clicker2 } from "./ejemplos/Clicker2"
-import Nosotros from "./ejemplos/Nosotros"
-import { useEffect, useState } from "react"
-
 function App() {
-
-  const [mostrar, setMostrar] = useState(true)
-
-  const handleMostrar = () => {
-    setMostrar(!mostrar)
-  }
 
   return (
     <div>
       <Header />
-
-      <button onClick={handleMostrar}>Mostrar</button>
-
-      {
-        mostrar ? <Clicker2 /> : null
-      }
-
+      <ItemListContainer />
     </div>
    
   )
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,66 @@
+import { useState } from 'react'
 import './ItemListContainer.css'
+import { useEffect } from 'react'
+import { pedirDatos } from '../../helpers/pedirDatos'
+import ItemList from '../ItemList/ItemList'
 
-const ItemListContainer = ( {mensaje} ) => {
+const ItemListContainer = () => {
+
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        pedirDatos()
+            .then((res) => {
+                setProductos(res)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
 
     return (
-        <div className="list__container">
-            <h2>Item List Container</h2>
-            <hr/>
-
-            <p>{mensaje}</p>
+        <div className="container my-5">
+           <ItemList items={productos}/>
         </div>
     )
 }
 
 export default ItemListContainer
+
+
+// const promesa = new Promise((resolve, reject) => {
+//     // cuerpo promesa
+//     setTimeout(() => {
+//         // resolve("Promesa resuelta")
+//         reject("Promesa rechazada")
+//     }, 5000)
+// })
+
+// promesa
+//     .then(() => {
+//         console.log("Hola mundo")
+//     })
+//     .catch(() => {
+//         console.log("Chau mundo")
+//     })
+
+
+// const procesoAsync = (bool) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (bool) {
+//                 resolve("Juan Carlos")
+//             } else {
+//                 reject("Promesa rechazada")
+//             }
+//         }, 2000)
+//     })
+// }
+
+// procesoAsync(true)
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
